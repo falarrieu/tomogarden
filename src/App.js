1a259bb08e1a0dee54b95dbbc0a176d104f5b58a
falarrieu <federicolarrieu@outlook.com>
2023-02-27 19:53:27 -0700

    Base Code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import './css/App.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Market from "./Market/Market";
+import Layout from "./Layout";
+import Social from "./Social/Social";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route path="/Social" element={<Social/>}/>
+                    <Route path="/Market" element={<Market/>}/>
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
-export default App;
---
+++ src/Social/Social.js
@@ -0,0 +1,8 @@
+
+export default function Social() {
+    return(
+        <div>
+            You made it to the social page!
+        </div>
+    )
+}
